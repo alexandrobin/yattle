03c47ab8282bae95ce028b8329cfec0bbff04cd4
Alexandre Robin <alexandre.robin2@ubisoft.com>
2019-01-18 18:28:37 +0100

    Updated icons & added sound when task is done

--- src/components/fontawesome.js
+++ src/components/fontawesome.js
@@ -1,6 +1,6 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheckSquare, faCoffee, faTrash, fas } from '@fortawesome/free-solid-svg-icons'
-
-library.add( faCheckSquare, faCoffee, faTrash, fas)
+import { faCheckSquare, faCoffee, faTrash, fas} from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+library.add( faCheckSquare, faCoffee, faTrash, fas, far)
 
 export default library
